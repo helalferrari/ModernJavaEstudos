0be010084da1f4310a6233d4435a1fc82919146d
Helal Ferrari <ferrari.work@gmail.com>
2025-12-11 17:30:59 -0300

    teste

---
+++ src/Estudos/Dio1.java
@@ -0,0 +1,10 @@
+package Estudos;
+
+public class Dio1 {
+    static void main(String[] args) {
+        System.out.println("paro aqui");
+        int x=10;
+        int y=25;
+        System.out.println("paro aqui2");
+    }
+}
